334da18674af65f5646bcfc87cb2bc8d8f23293d
Phua Kuan Yu <Phuaky@virus.local>
2016-10-05 16:27:46 +0800

    added forms, bcrypt

--- clinicapp/app/models/user.rb
+++ clinicapp/app/models/user.rb
@@ -1,2 +1,21 @@
 class User < ApplicationRecord
+  validates :email,
+  presence: true,
+  uniqueness: {case_sensitive: false}
+
+  validates :password,
+  confirmation: true,
+  length: { in: 8..72 },
+  on: :create
+
+  validates :password_confirmation,
+  length: { in: 8..72 },
+  presence: true,
+  on: :create
+
+  has_secure_password
+
+  def self.authenticate(params)
+    User.find_by_email(params[:email]).try(:authenticate, params[:password])
+  end
 end
