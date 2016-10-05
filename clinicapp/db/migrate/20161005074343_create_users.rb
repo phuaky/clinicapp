e58f8318da140241941c6fbde5e51a0ee486b0da
Phua Kuan Yu <Phuaky@virus.local>
2016-10-05 16:13:08 +0800

    homepage up

--- clinicapp/db/migrate/20161005074343_create_users.rb
+++ clinicapp/db/migrate/20161005074343_create_users.rb
@@ -2,9 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       t.string :email
-      t.string :name
       t.string :password_digest
-      t.string :user_type
 
       t.timestamps
     end
--- clinicapp/db/schema.rb
+++ clinicapp/db/schema.rb
@@ -17,9 +17,7 @@ ActiveRecord::Schema.define(version: 20161005074343) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
-    t.string   "name"
     t.string   "password_digest"
-    t.string   "user_type"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
