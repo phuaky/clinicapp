8cc30f41a1c4494d1957db75ca854f6a8da47fa6
Phua Kuan Yu <Phuaky@virus.local>
2016-10-06 19:45:54 +0800

    hello

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :is_admin, only: [:index]
+  before_action :is_admin, only: [:index, :new]
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
